b9b62375693b5ddd690da52c1a8a9dfcb5e2d316
Wiamtwt77 <124047244+Wiamtwt77@users.noreply.github.com>
2026-08-15 16:41:41 +0200

    Add getEasingProgress function for easing calculations

--- tween-craft-upload-studio/src/animation/easing.js
+++ tween-craft-upload-studio/src/animation/easing.js
@@ -1,1 +1,7 @@
 
+export function getEasingProgress(t, type = 'easeInOut') {
+    if (type === 'easeInOut') {
+        return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+    }
+    return t;
+}
