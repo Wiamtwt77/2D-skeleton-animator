d93a30ec18d6333600505386a7a6e137c8615ba8
Wiamtwt77 <124047244+Wiamtwt77@users.noreply.github.com>
2026-08-15 16:41:57 +0200

    Update timeline.js

--- tween-craft-upload-studio/src/animation/timeline.js
+++ tween-craft-upload-studio/src/animation/timeline.js
@@ -1,1 +1,38 @@
 
+export class TimelineEngine {
+    constructor() {
+        this.keyframes = [];
+        this.currentFrameIdx = 0;
+        this.isPlaying = false;
+        this.animProgress = 0;
+    }
+
+    saveKeyframe(bones) {
+        let pose = bones.map(b => ({ x: b.x, y: b.y, angle: b.angle }));
+        this.keyframes.push(pose);
+        return this.keyframes.length;
+    }
+
+    clear() {
+        this.keyframes = [];
+        this.currentFrameIdx = 0;
+        this.isPlaying = false;
+    }
+
+    applyPose(bones, pose) {
+        pose.forEach((p, i) => {
+            bones[i].x = p.x;
+            bones[i].y = p.y;
+            bones[i].angle = p.angle;
+        });
+    }
+
+    interpolate(poseA, poseB, t, bones) {
+        poseA.forEach((pA, i) => {
+            let pB = poseB[i];
+            bones[i].x = pA.x + (pB.x - pA.x) * t;
+            bones[i].y = pA.y + (pB.y - pA.y) * t;
+            bones[i].angle = pA.angle + (pB.angle - pA.angle) * t;
+        });
+    }
+}
